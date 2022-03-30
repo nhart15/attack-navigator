42a622085f013b07381764b3aa43b3a746a8633a
nhart <nhart@mitre.org>
2022-03-30 10:13:29 -0400

    Massive changes

--- nav-app/src/assets/generate_stix_atlas.py
+++ nav-app/src/assets/generate_stix_atlas.py
@@ -48,7 +48,7 @@ class ATLAS:
             atlas_data (str): Dictionary of ATLAS.yaml data
         """
         self.parse_data_files(atlas_data)
-        # Track ATLAS approaches by short ID for matrix ordering lookup
+        # Track ATLAS tactics by short ID for matrix ordering lookup
         self.tactic_mapping = {}
 
     def parse_data_files(self, atlas_data):
@@ -58,43 +58,43 @@ class ATLAS:
         self.matrix_name = atlas_data["name"]
         self.matrix_version = atlas_data["version"]
 
-        self.approaches = atlas_data["approaches"]
-        self.activities = atlas_data["activities"]
+        self.tactics = atlas_data["tactics"]
+        self.techniques = atlas_data["techniques"]
         #self.studies = atlas_data["case-studies"]
 
     def to_stix_json(self, stix_output_filepath, atlas_url):
-        """Saves a STIX JSON file of the ATLAS approaches and activities info.
+        """Saves a STIX JSON file of the ATLAS tactics and techniques info.
 
         STIX Bundle specs
         https://docs.oasis-open.org/cti/stix/v2.1/cs01/stix-v2.1-cs01.html#_nuwp4rox8c7r
         """
 
-        # Convert ATLAS activities first to populate the referenced ATT&CK approaches
-        # Only for parent activities, as subactivities do not have approaches references
-        stix_activities = []
+        # Convert ATLAS techniques first to populate the referenced ATT&CK tactics
+        # Only for parent techniques, as subtechniques do not have tactics references
+        stix_techniques = []
         relationships = []
         parent_technique = None
-        for t in self.activities:
+        for t in self.techniques:
             if 'subtechnique-of' in t:
                 pass
                 # Create subtechnique and relationship
                 subtechnique, relationship = self.subtechnique_to_attack_pattern(t, parent_technique, atlas_url)
                 # Add to trackers
-                stix_activities.append(subtechnique)
+                stix_techniques.append(subtechnique)
                 relationships.append(relationship)
             else:
                 # Create and add this technique
                 technique = self.technique_to_attack_pattern(t, atlas_url)
-                stix_activities.append(technique)
-                # Save off reference to this technique for use by its subactivities, should there be any following
+                stix_techniques.append(technique)
+                # Save off reference to this technique for use by its subtechniques, should there be any following
                 parent_technique = technique
 
-        print(f'Converted {len(stix_activities)} ATLAS activities to STIX objects.')
+        print(f'Converted {len(stix_techniques)} ATLAS techniques to STIX objects.')
         print(f'Created {len(relationships)} subtechnique relationships.')
 
-        # Convert ATLAS approaches to x-mitre-approaches
-        stix_approaches = [self.tactic_to_mitre_attack_tactic(t, atlas_url) for t in self.approaches]
-        print(f'Converted {len(stix_approaches)} ATLAS approaches to STIX objects.')
+        # Convert ATLAS tactics to x-mitre-tactics
+        stix_tactics = [self.tactic_to_mitre_attack_tactic(t, atlas_url) for t in self.tactics]
+        print(f'Converted {len(stix_tactics)} ATLAS tactics to STIX objects.')
 
 
         # Build x-mitre-matrix
@@ -108,11 +108,11 @@ class ATLAS:
             )
         ]
 
-        # Build ordered list of approaches
+        # Build ordered list of tactics
         tactic_refs = []
 
-        # Order of approaches in matrix, by STIX ID reference
-        tactic_refs = [self.tactic_mapping[tactic['id']]['id'] for tactic in self.approaches]
+        # Order of tactics in matrix, by STIX ID reference
+        tactic_refs = [self.tactic_mapping[tactic['id']]['id'] for tactic in self.tactics]
 
         print(f'Generated {len(tactic_refs)} tactic references for the ATLAS matrix object.')
 
@@ -126,7 +126,7 @@ class ATLAS:
         # JSON
         print('Bundling and serializing ATLAS data to JSON file...')
         bundle = Bundle(
-            objects=stix_approaches + stix_activities + relationships + [stix_matrix_obj],
+            objects=stix_tactics + stix_techniques + relationships + [stix_matrix_obj],
             allow_custom=True # Needed as ATT&CK data has custom objects
         )
         stix_json = json.loads(bundle.serialize())
@@ -136,7 +136,7 @@ class ATLAS:
             json.dump(stix_json, f)
             print(f'Done! See {stix_output_filepath}\n')
 
-    def referenced_approaches_to_kill_chain_phases(self, tactic_ids):
+    def referenced_tactics_to_kill_chain_phases(self, tactic_ids):
         """Converts a list of tactic IDs referenced by a technique
         to a list of STIX Kill Chain Phases.
 
@@ -155,7 +155,7 @@ class ATLAS:
                 kill_chain_name = ATLAS.SOURCE_NAME # Using this as an identifier
 
                 # Look up ATLAS tactic name
-                tactic = next((tactic for tactic in self.approaches if tactic['id'] == tactic_id), None)
+                tactic = next((tactic for tactic in self.tactics if tactic['id'] == tactic_id), None)
                 # Ensure this is found
                 assert(tactic is not None)
                 # Convert name to lowercase and hyphens to fit spec
@@ -170,7 +170,7 @@ class ATLAS:
 
         return kill_chain_phases
 
-    def build_atlas_external_references(self, t, atlas_url, route='activities'):
+    def build_atlas_external_references(self, t, atlas_url, route='techniques'):
         """Returns a STIX External Reference for ATLAS data."""
 
         # Construct the full URL to the resource
@@ -190,7 +190,7 @@ class ATLAS:
         at = AttackTactic(
             name=t['name'],
             description=t['description'],
-            external_references=self.build_atlas_external_references(t, atlas_url, 'approaches'),
+            external_references=self.build_atlas_external_references(t, atlas_url, 'tactics'),
             x_mitre_shortname=t['name'].lower().replace(' ','-'),
         )
 
@@ -204,7 +204,7 @@ class ATLAS:
         return AttackPattern(
             name=t['name'],
             description=t['description'],
-            kill_chain_phases=self.referenced_approaches_to_kill_chain_phases(t['approaches']),
+            kill_chain_phases=self.referenced_tactics_to_kill_chain_phases(t['tactics']),
             external_references=self.build_atlas_external_references(t, atlas_url),
             # Needed by Navigator else TypeError technique.platforms is not iterable
             allow_custom=True,
@@ -215,7 +215,7 @@ class ATLAS:
         """Returns a STIX AttackPattern representing this subtechnique and a STIX Relationship
         between this subtechnique and its parent.
 
-        https://github.com/mitre/cti/blob/master/USAGE.md#sub-activities
+        https://github.com/mitre/cti/blob/master/USAGE.md#sub-techniques
         """
         subtechnique = AttackPattern(
             name=t['name'],
@@ -241,7 +241,7 @@ if __name__ == '__main__':
     """Main entry point to STIX file generation for ATLAS data."""
 
     parser = ArgumentParser(
-        description="Creates a STIX JSON file showing approaches and activities used by ATLAS."
+        description="Creates a STIX JSON file showing tactics and techniques used by ATLAS."
     )
     parser.add_argument("-f",
         type=str,
@@ -268,7 +268,7 @@ if __name__ == '__main__':
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     # Output filepath
-    stix_output_filepath =  output_dir / 'stix-engage.json'
+    stix_output_filepath =  output_dir / 'stix-atlas.json'
 
     with open(args.atlas_data_filepath) as f:
         # Load in ATLAS data
